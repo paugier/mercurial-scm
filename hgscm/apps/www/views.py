cbf6e24dbcfd3b6dcc79bcc22c3e8132274c7e1f
David Soria Parra <dsp@php.net>
2009-02-17 12:54:07 +0100

    about: integrate about page in django version

--- hgscm/apps/www/views.py
+++ hgscm/apps/www/views.py
@@ -4,3 +4,6 @@ from django.template import RequestContext
 def frontpage(request):
     return render_to_response("frontpage.html", { },
         RequestContext(request))
+def about(request):
+    return render_to_response("about.html", { },
+        RequestContext(request))
