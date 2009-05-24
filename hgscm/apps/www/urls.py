d66f4ded688c078c12f3f4ca743b6d0f5d3ae739
Arne Babenhauserheide <bab@draketo.de>
2009-05-24 16:05:17 +0200

    changed guide url to /workflow-guide.

--- hgscm/apps/www/urls.py
+++ hgscm/apps/www/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('hgscm.apps.www.views',
     url(r'^$', 'frontpage', name='frontpage'),
     url(r'^about$', 'about', name='about'),
-    url(r'^workflow_guide$', 'workflow_guide', name='workflow_guide'),
+    url(r'^workflow-guide$', 'workflow_guide', name='workflow_guide'),
     url(r'^thepage$', 'thepage', name='thepage'),
     url(r'^downloads$', 'downloads', name='downloads'),
     url(r'^download/(?P<version>.*?)/(?P<platform>.*?)$', 'download', name='download'),
