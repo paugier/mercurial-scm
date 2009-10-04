ee05485e2468762f1f22689d0766f46d70a2acba
David Soria Parra <dsp@php.net>
2009-10-04 05:03:28 +0200

    Working version of the download button with simple OS detection

--- static/javascript/download.js
+++ static/javascript/download.js
@@ -1,10 +1,28 @@
-function os_detection() {
-    var OSName="Source";
-    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="Mac OS X";
-    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-
-    document.write(OSName);
+function Download() {
+    this.downloads = new Array();
 }
 
+Download.prototype = {
+
+    parse_os: function() {
+        var OSName="Source code";
+        if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+        if (navigator.appVersion.indexOf("Mac")!=-1) OSName="Mac OS X";
+        if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+        if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+        return OSName;
+    },
+
+    os_detection: function() {
+        document.write(this.parse_os());
+    },
+
+    os_link: function() {
+        document.location.href = this.downloads[this.parse_os()];
+    },
+
+    register_download:function(type, url) {
+        this.downloads[type] = url;
+    }
+}
