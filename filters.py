764c1353f8e4c3974ca4fe4dd85b46760dcd8697
David Soria Parra <davidsp@fb.com>
2014-03-07 14:47:13 -0800

    Use flask to render site and get rid of submodules
    We don't want to use statically generated html files anymore. We are using
    flask to do the routing and render the templates for now. This means we also
    get rid of the submoduels and put everything together in templates/.

--- filters.py
+++
@@ -1,7 +0,0 @@
-import markdown as _markdown
-from jinja2 import Markup
-
-md = _markdown.Markdown(extensions=['toc'], safe_mode=False)
-
-def mdown(value):
-    return Markup(md.convert(value))
---
+++ run.py
@@ -0,0 +1,30 @@
+#
+# (c) 2014 David Soria Parra <dsp@php.net>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+import os
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def indexpage():
+    return flask.render_template('frontpage.html')
+
+
+@app.route('/<site>')
+def about(site=None):
+    if not site:
+        flask.abort(404)
+    tpath = os.path.join('templates', site, 'index.html')
+    if not os.path.exists(tpath):
+        flask.abort(404)
+    t = os.path.join(site, 'index.html')
+    return flask.render_template(t)
+
+
+app.debug = True
+if __name__ == '__main__':
+    app.run()
