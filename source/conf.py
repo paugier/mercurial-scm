82fea374bb8a8af3c3895805b38660b13de105a9
Raphaël Gomès <rgomes@octobus.net>
2025-02-10 11:59:18 +0100

    css: rename to `hg-custom`
    Less chance of "something magical" happening under our feet.

--- source/conf.py
+++ source/conf.py
@@ -130,7 +130,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+html_css_files = ["hg-custom.css"]
 
 # copybutton_selector = "div:not(.output) > div.highlight pre"
 
